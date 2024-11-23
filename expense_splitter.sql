b7b88ce4e596ee54a021e3dfcbbf36daa6ab8a1f
Mary Yang <66921052+goldfishtelbooth@users.noreply.github.com>
2024-11-22 16:59:51 -0800

    remove unrelated files

--- expense_splitter.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS expense_splitter;
-CREATE DATABASE expense_splitter;
-Use expense_splitter;
-
-select * from expense_groups;
-
-select * from participant;
-
-select * from group_participants;
